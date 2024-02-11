4c5c3a87c723fb376dec3a35179a861b9a76770d
Gabriel Trigo <gabrielguerratrigo20@gmail.com>
2024-02-11 13:50:02 -0500

    Almost done with article blocks

--- src/components/LeftArticle.js
+++ src/components/LeftArticle.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import Red from "../images/Red.svg"
+import Yellow from "../images/Yellow.svg"
+import Green from "../images/Green.svg"
+import SmallGreen from "../images/SmallGreen.svg"
 
 const LeftArticleWrapper = styled.div`
     max-width: 70vw;
     margin: 15px;
     display: flex;
-    background-color: #7A5470;
     z-index: 1;
 
     @media screen and (max-width: 1023px) {
@@ -17,49 +20,73 @@ const LeftArticleWrapper = styled.div`
 `;
 
 const Column = styled.div`
-    flex: 1;
-    height: fit-content;
+    display: flex;
+    align-items: center;
+    width: 50%;
     @media screen and (max-width: 1023px) {
         width: 100%;
     }
 `;
 
 const MainImage = styled.div`
-    width: 350px;
-    height: 350px;
+    max-width: 100%;
+    max-height: 100%;
     z-index: 2;
-    margin: 150px 0px 75px 90px;
-    background: #907388;
+    position: relative;
 
     img {
         width: 100%;
         height: 100%;
-        margin: -75px 0px 0px -75px;
         object-fit: cover;
     }
 
     @media screen and (max-width: 1023px) {
-        width: 250px;
-        height: 250px;
+        max-width: 100%;
+        max-height: 100%;
     }
 
     @media screen and (max-width: 768px) {
-        width: 30vw;
-        height: 30vw;
-        margin: 100px 0px 30px 90px;
+        max-width: 100%;
+        max-height: 100%;
 
         img {
-            margin: -50px 0px 0px -50px;
+            margin: 0px 0px 0px 0px;
         }
     }
 `;
+
+const TitleImage = styled.div`
+    display: flex;
+    z-index: 3;
+    max-width: 100%;
+    margin: 0rem 0rem 0rem -10%;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+const ArticleImage = styled.div`
+    position: absolute;
+    max-height: 80%;
+    max-width: 70%;
+    overflow: hidden;
+    top: 10%;
+    left: 10%;
+    border-radius: 10px;
+`
 
 const ArticleInfo = styled.div`
     display: flex;
     flex-direction: column;
     z-index: 1;
     width: fit-content;
-    margin: 2rem 1rem 0rem -1rem;
+    margin: 0rem 0rem 0rem 0rem;
     
     font-family: 'EB Garamond';
     text-transform: capitalize;
@@ -68,9 +95,32 @@ const ArticleInfo = styled.div`
 `;
 
 const ArticleTitle = styled(ArticleInfo)`
-    font-size: 1.8rem;
+    position: absolute;
+    font-size: 1.4rem;
+    padding: 10%;
+    @media screen and (max-width: 1200px) {
+        font-size: 1.1rem;
+    }
+    @media screen and (max-width: 1023px) {
+        font-size: 1.1rem;
+    }
     @media screen and (max-width: 768px) {
-        font-size: 1rem;
+        font-size: 0.9rem;
+    }
+    @media screen and (max-width: 600px) {
+        font-size: 0.8rem;
+    }
+    @media screen and (max-width: 550px) {
+        font-size: 0.7rem;
+    }
+    @media screen and (max-width: 500px) {
+        font-size: 0.6rem;
+    }
+    @media screen and (max-width: 450px) {
+        font-size: 0.5rem;
+    }
+    @media screen and (max-width: 400px) {
+        font-size: 0.4rem;
     }
 `;
 
@@ -88,14 +138,17 @@ const LeftArticle = ({ article }) => {
         <LeftArticleWrapper>     
             <Column>
                 <MainImage>
-                    <img alt={article.article_title} src={article.image_url}/>
+                    <img alt={article.article_title} src={Red}/>
+                    <ArticleImage>
+                        <img src={article.image_url}/>
+                    </ArticleImage>
                 </MainImage>
             </Column>
             <Column>
-                <ArticleInfo>
+                <TitleImage>
+                    <img alt={article.article_title} src={SmallGreen}/>
                     <ArticleTitle>{article.article_title}</ArticleTitle>
-                    <ArticleAuthor>By {article.article_authors}</ArticleAuthor>
-                </ArticleInfo>
+                </TitleImage>
             </Column>
         </LeftArticleWrapper>
     );
